49d7878a830dea1c5da05b1a3ae7332e368640e5
Ben U <sluggyben@hotmail.com>
2019-06-01 22:53:11 -0400

    Pentano

--- pentano/dialogue.js
+++ pentano/dialogue.js
@@ -2,8 +2,8 @@ const dialogue = {
 	'tutorial001': {
 		'perams' : {'last_index' : 2, 'endgoto' : 'defhome'},
 		'text': [
-			['Testman', 'diopfps/def.png', 'Greetings. This is a test.'],
-			['Testman', 'diopfps/def.png', 'This is a message to you.'],
+			['Testman', 'diopfps/def.png', 'Hello. Click on the dialogue box to continue.'],
+			['Testman', 'diopfps/def.png', 'This is a dialogue test.'],
 			['Testman', 'diopfps/def.png', 'After this, you will go to the main menu.']
 		]
 	}
