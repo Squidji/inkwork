96079a0d85f3f0128e74ccab7ee31a3c6f1e0a96
Ben U <sluggyben@hotmail.com>
2019-06-04 21:22:28 -0400

    Pentano

--- pentano/data/dialogue.js
+++ pentano/data/dialogue.js
@@ -1,10 +1,11 @@
 const dialogue = {
 	'test000': {
-		'perams' : {'last_index' : 2, 'endgoto' : 'defhome'},
+		'perams' : {'last_index' : 2, 'endgoto' : 'explore'},
 		'text': [
 			['Testman', 'diopfps/def.png', 'Hello. Click on the dialogue box to continue.'],
 			['Testman', 'diopfps/def.png', 'This is a dialogue test.'],
-			['Testman', 'diopfps/def.png', 'After this, you will go to the main menu.']
+			['Testman', 'diopfps/def.png', 'You will now go to Tutoria. Be aware ' +
+			'that the game is still under development']
 		]
 	}
 };
